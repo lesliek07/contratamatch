5fcb233abb7682fd54cf7dbbf9494b3b77bdc859
Leslie Figueroa <lesliek.x1999@gmail.com>
2024-07-11 01:06:16 -0400

    se arregla el menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 const root= document.querySelector('#root')
 const toggler= document.querySelector('#toggler')
+const cerrar = document.querySelector('#cerrar')
+const menuLinks = document.querySelectorAll('#lista a')
 
 toggler.addEventListener('click',()=>{
-    root.classList.toggle('open')
+    root.classList.add('open')
+})
+cerrar.addEventListener('click', ()=>{
+    root.classList.remove('open')
+})
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        root.classList.remove('open');
+    });
 })
